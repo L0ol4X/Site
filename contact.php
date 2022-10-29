0c48ea54b40db63adbdfca8cd4d974cc35739381
L0ol4X <lolachalmin@gmail.com>
2022-10-29 13:36:39 +0200

    actualisation data

--- contact.php
+++ contact.php
@@ -1,2 +1,17 @@
-<?php 
-	echo($_POST["name"]);
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="zelda.css" type="text/css">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Contact</title>
+	</head>
+	
+	<a class="button" href="index.html">Home</a>
+ 	<div class="php">
+	 	<?php
+			echo("<p>Merci de votre retour !</p>");
+		?>
+	</div>
+</html>
+
